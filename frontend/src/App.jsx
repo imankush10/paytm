5336969f76286ed476b406c8171f524f8c041392
Ankush Kumar <imankush1010@gmail.com>
2024-07-14 07:13:32 +0530

    Lazy loading, Suspense

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,44 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Signin from "./pages/Signin";
-import Dashboard from "./pages/Dashboard";
-import SendMoney from "./pages/SendMoney";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
 import AuthContext from "./context/AuthContext";
-import ProtectedRoutes from "./pages/ProtectedRoutes";
 import UsersContext from "./context/UsersContext";
+import SpinnerFullPage from "./components/SpinnerFullPage/SpinnerFullPage";
+
+const Signup = lazy(() => import("./pages/Signup"));
+const Signin = lazy(() => import("./pages/Signin"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const SendMoney = lazy(() => import("./pages/SendMoney"));
+const ProtectedRoutes = lazy(() => import("./pages/ProtectedRoutes"));
 
 function App() {
   return (
     <AuthContext>
       <UsersContext>
         <BrowserRouter>
-          <Routes>
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoutes>
-                  <Dashboard />
-                </ProtectedRoutes>
-              }
-            />
-            <Route
-              path="/sendmoney"
-              element={
-                <ProtectedRoutes>
-                  <SendMoney />
-                </ProtectedRoutes>
-              }
-            />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route path="/" element={<Navigate replace to="/signin" />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoutes>
+                    <Dashboard />
+                  </ProtectedRoutes>
+                }
+              />
+              <Route
+                path="/sendmoney"
+                element={
+                  <ProtectedRoutes>
+                    <SendMoney />
+                  </ProtectedRoutes>
+                }
+              />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </UsersContext>
     </AuthContext>
